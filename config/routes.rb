87cc548b88067c45b22eed97733c2fcd12393920
Bryan Mayes <bmayes@gmail.com>
2013-12-13 15:47:56 -0800

    What the is a commit.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ TestRest::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  # that the?
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
