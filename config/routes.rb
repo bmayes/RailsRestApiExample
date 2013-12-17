a74fcbbcbca532dd80e73d7e7bb5ccffb2616eec
Bryan Mayes <bmayes@gmail.com>
2013-12-17 00:53:14 -0800

    Added another comment.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ TestRest::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # Well sh00t.......
+  # Nice.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
