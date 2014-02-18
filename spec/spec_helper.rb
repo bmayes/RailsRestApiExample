35c5ee4dff8dc003f4d9b799a2b3ba42df93b487
Bryan Mayes <bmayes@gmail.com>
2014-02-18 12:01:16 -0800

    Update spec_helper.rb
    adding comment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
   # fake a change.
   # comment 2
+  # this is not a test.
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
